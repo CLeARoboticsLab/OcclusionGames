56f1e54d7db82202f0f1557beaaffd864ebb009e
Jai Nagaraj <jai.v.nagaraj@gmail.com>
2025-07-14 15:48:55 -0500

    Finished Jetson TCP server i think

--- scripts/tcp_server.py
+++ scripts/tcp_server.py
@@ -2,8 +2,10 @@
 
 """
 Author: Jai Nagaraj
-This Python scipt sets up a TCP server for a JetRacer to receive steering and throttle data.
+This Python scipt sets up a TCP server for a JetRacer to receive steering and throttle data,
+as well as send out pose data for the JetRacer.
 Data is expected to be sent as: {"throttle": <float>, "steering": <float>}
+Data from this server is sent as: {"x": <float>, "y": <float>, "v": <float>, "psi": <float>}
 """
 
 import socket
@@ -11,19 +13,50 @@ import threading
 import json
 import rospy
 from std_msgs import Float32
-from geometry_msgs import PoseStamped
+from geometry_msgs import PoseStamped, TwistStamped
+import math
 import time
 
 HOST = '0.0.0.0'  # localhost
 PORT = 65432        # non-privileged port
 
 POSE_TOPIC = "vrpn_client_node/JaiAliJetRacer/pose"
+TWIST_TOPIC = "vrpn_client_node/JaiAliJetRacer/twist"
 THROTTLE_TOPIC = "/jetracer/throttle"
 STEERING_TOPIC = "/jetracer/steering"
 
 throttle_pub = None
 steering_pub = None
 pose_sub = None
+latest_v = 0
+
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+    
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+    
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+    
+    return roll_x, pitch_y, yaw_z # in radians
+
+def twist_callback(data: TwistStamped):
+    global latest_v
+    v_x = data.twist.linear.x
+    v_y = data.twist.linear.y
+    latest_v = math.hypot(v_x, v_y)
 
 def handle_receive(conn):
     while True:
@@ -36,27 +69,35 @@ def handle_receive(conn):
         print(f"Throttle received: {json_obj["steering"]}\n")
 
         # output throttle, steering to jetracer
-        throttle_pub.publish()
+        throttle_pub.publish(Float32(float(json_obj["throttle"])))
+        steering_pub.publish(Float32(float(json_obj["steering"])))
 
-def handle_send(conn, data):
-    while True:
-        msg = input("[Server]: ")
-        conn.sendall(msg.encode())
+def handle_send(data: PoseStamped, args):
+    conn = args[0]
+    # create the JSON object
+    psi = euler_from_quaternion(data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.w,data.pose.orientation.z)
+    dict_to_send = {"x": data.pose.position.x, "y": data.pose.position.y, "v": latest_v, "psi": psi}
+    json_str_to_send = json.dumps(dict_to_send)
+    conn.sendall(json_str_to_send.encode())
 
 def main():
+    global throttle_pub, steering_pub, pose_sub
     rospy.init_node("tcp_jetracer_server")
     throttle_pub = rospy.Publisher(THROTTLE_TOPIC, Float32, queue_size=1)
     steering_pub = rospy.Publisher(STEERING_TOPIC, Float32, queue_size=1)
-    pose_sub = rospy.Subscriber(POSE_TOPIC, PoseStamped, handle_send)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         print(f"Server listening on {HOST}:{PORT}...")
         conn, addr = s.accept()
         print(f"Connected by {addr}")
+        # now that we have a connection, create pose and twist subscriber
+        pose_sub = rospy.Subscriber(POSE_TOPIC, PoseStamped, handle_send, callback_args=(conn))
+        pose_sub = rospy.Subscriber(TWIST_TOPIC, TwistStamped, twist_callback)
 
         threading.Thread(target=handle_receive, args=(conn,), daemon=True).start()
         handle_send(conn)
+        rospy.spin()
 
 if __name__ == "__main__":
     main()
